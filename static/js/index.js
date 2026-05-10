b9e030200c95bdec91615328ec1789cccabc5406
Jiayun Li <lijiayun2016@gmail.com>
2026-05-10 17:33:16 +0200

    Create Allegro video only on desktop

--- static/js/index.js
+++ static/js/index.js
@@ -42,29 +42,41 @@ document.querySelectorAll('video[data-playback-rate]').forEach(video => {
   video.addEventListener('play', setPlaybackRate);
 });
 
-document.querySelectorAll('video[data-mobile-src][data-desktop-src]').forEach(video => {
+document.querySelectorAll('.allegro-video-mount[data-video-src]').forEach(mount => {
   const desktopQuery = window.matchMedia('(min-width: 901px)');
 
-  const setResponsiveSource = () => {
-    const src = desktopQuery.matches ? video.dataset.desktopSrc : video.dataset.mobileSrc;
-
-    if (!src || video.getAttribute('src') === src) {
+  const syncDesktopVideo = () => {
+    if (!desktopQuery.matches) {
+      mount.replaceChildren();
       return;
     }
 
-    video.setAttribute('src', src);
-    video.load();
-
-    if (video.autoplay) {
-      video.play().catch(() => {});
+    if (mount.querySelector('video')) {
+      return;
     }
+
+    const video = document.createElement('video');
+    video.className = 'paper-video allegro-video-desktop';
+    video.src = mount.dataset.videoSrc;
+    video.poster = mount.dataset.posterSrc || '';
+    video.autoplay = true;
+    video.muted = true;
+    video.defaultMuted = true;
+    video.loop = true;
+    video.playsInline = true;
+    video.setAttribute('playsinline', '');
+    video.setAttribute('webkit-playsinline', '');
+    video.preload = 'metadata';
+    mount.append(video);
+
+    video.play().catch(() => {});
   };
 
-  setResponsiveSource();
+  syncDesktopVideo();
   if (typeof desktopQuery.addEventListener === 'function') {
-    desktopQuery.addEventListener('change', setResponsiveSource);
+    desktopQuery.addEventListener('change', syncDesktopVideo);
   } else {
-    desktopQuery.addListener(setResponsiveSource);
+    desktopQuery.addListener(syncDesktopVideo);
   }
 });
 
